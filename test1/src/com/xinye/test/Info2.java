f3c87025af4edc71009d30a6283a42d8c9885997
王滨 <651198577@qq.com>
2019-10-28 11:58:56 +0800

    info2第一次提交

---
+++ test1/src/com/xinye/test/Info2.java
@@ -0,0 +1,10 @@
+package com.xinye.test;
+
+/**
+ * Created by Administrator on 2019/10/28.
+ */
+public class Info2 {
+    public static void main(String[] args) {
+        System.out.println("info2");
+    }
+}
